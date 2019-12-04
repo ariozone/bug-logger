eb3eb809b66281695a0934980fce517b532517b2
Ario Tamaddoni <ariozone6@gmail.com>
2019-12-03 19:58:06 -0800

    Added material icons and css.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./App.css"
+import "materialize-css/dist/css/materialize.min.css"
+import M from "materialize-css/dist/js/materialize.min.js"
 
 function App() {
+  useEffect(() => {
+    M.AutoInit()
+  })
   return <div className='App'>Bug Logger</div>
 }
 
