63e919dcb1353b79aa1f93ebcce59dbb3f9b42d2
Ario Tamaddoni <ariozone6@gmail.com>
2019-12-05 20:23:08 -0800

    Create SearchBar component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useEffect } from "react"
 import "./App.css"
 import "materialize-css/dist/css/materialize.min.css"
 import M from "materialize-css/dist/js/materialize.min.js"
+import SearchBar from "./components/layout/SearchBar"
 
 function App() {
   useEffect(() => {
     M.AutoInit()
   })
-  return <div className='App'>Bug Logger</div>
+  return (
+    <div className='App'>
+      <SearchBar></SearchBar>
+    </div>
+  )
 }
 
 export default App
