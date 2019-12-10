3cc9cf686d220e406ef1d00d718e1ae1df275c3f
Ario Tamaddoni <ariozone6@gmail.com>
2019-12-09 19:51:07 -0800

    Enhanced Logs components t render list of log messages using hooks.

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,7 +1,31 @@
 import React from "react"
+import axios from "axios"
 
 const Logs = () => {
-  return <div>Logs</div>
+  const [logs, setLogs] = React.useState([])
+  const [spinner, setSpinner] = React.useState(false)
+  const getLogs = async () => {
+    setSpinner(true)
+    const res = await axios.get("/logs")
+    setLogs(res.data)
+    setSpinner(false)
+  }
+  React.useEffect(() => {
+    getLogs()
+  }, [])
+  if (spinner) return <h2>Loading...</h2>
+  return (
+    <div>
+      <ul className='collection-with-header'>
+        <li className='collection-header'>
+          <h3 className='center'>Logged Defects</h3>
+        </li>
+        {logs.map(log => (
+          <li className='center'>{log.message}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Logs
