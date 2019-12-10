5fe4df2ac9a55298a4284b0523df68fc95273ba3
Ario Tamaddoni <ariozone6@gmail.com>
2019-12-09 19:17:10 -0800

    Create Logs component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 import "materialize-css/dist/css/materialize.min.css"
 import M from "materialize-css/dist/js/materialize.min.js"
 import SearchBar from "./components/layout/SearchBar"
+import Logs from "./components/logs/Logs"
 
 function App() {
   useEffect(() => {
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className='App'>
       <SearchBar></SearchBar>
+      <Logs></Logs>
     </div>
   )
 }
